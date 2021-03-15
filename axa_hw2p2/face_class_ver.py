93626449079be3db469c37cc8baf9d2955f6d5c4
alejandroxag <alejandro.alvarez.1804@gmail.com>
2021-03-15 13:25:06 -0400

    changes

--- axa_hw2p2/face_class_ver.py
+++ axa_hw2p2/face_class_ver.py
@@ -34,30 +34,9 @@ from hyperoptimization import fit_predict
 # from axa_hw2p2.hyperoptimization import fit_predict
 
 # Cell
-def main(sample_size=None):
+def main(batch_size, sample_size=None):
 
     # Hyperparameters space
-
-    # space = {'n_in_ch_bn': hp.choice(label='n_in_ch_bn', options=[3]),
-    #          'ls_out_ch_bn': hp.choice(label='ls_out_ch_bn',
-    #                                    options=[[16, 24, 32, 64, 96, 160, 320]]),
-    #          'ls_n_rep_bn': hp.choice(label='ls_n_rep_bn',
-    #                                    options=[[1, 2, 3, 4, 3, 3, 1]]),
-    #          'ls_stride_bn': hp.choice(label='ls_stride_bn',
-    #                                    options=[[1, 2, 2, 2, 1, 2, 1]]),
-    #          'ls_exp_fct_t_bn': hp.choice(label='ls_exp_fct_t_bn',
-    #                                       options=[[1, 6, 6, 6, 6, 6, 6]]),
-    #          'n_embeddings': hp.choice(label='n_embeddings', options=[1280]),
-    #          'n_classes': hp.choice(label='n_classes', options=[100]),
-    #          'batch_size': scope.int(hp.choice(label='batch_size', options=[16])),
-    #          'lr': hp.loguniform(label='lr', low=np.log(5e-2), high=np.log(2e-1)),
-    #          'lr_decay': hp.choice(label='lr_decay', options=[0.98,0.99,1]),
-    #          'n_lr_decay_steps': hp.choice(label='n_lr_decay_steps', options=[1,2,4]),
-    #          'center_loss': hp.choice(label='center_loss', options=[True]),
-    #          'lr_cl': hp.choice(label='lr_cl', options=[0.5]),
-    #          'alpha_cl': hp.choice(label='alpha_cl', options=[0.01,0.1,1]),
-    #          'n_epochs': hp.choice(label='n_epochs', options=[25]),
-    #          'eval_steps': scope.int(hp.choice(label='eval_steps', options=[4])),}
 
     space = {'n_in_ch_bn': hp.choice(label='n_in_ch_bn', options=[3]),
              'ls_out_ch_bn': hp.choice(label='ls_out_ch_bn',
@@ -69,8 +48,8 @@ def main(sample_size=None):
              'ls_exp_fct_t_bn': hp.choice(label='ls_exp_fct_t_bn',
                                           options=[[1, 6, 6, 6, 6, 6, 6]]),
              'n_embeddings': hp.choice(label='n_embeddings', options=[1280]),
-             'n_classes': hp.choice(label='n_classes', options=[4000]),
-             'batch_size': scope.int(hp.choice(label='batch_size', options=[512])),
+            #  'n_classes': hp.choice(label='n_classes', options=[4000]),
+             'batch_size': scope.int(hp.choice(label='batch_size', options=[batch_size])),
              'lr': hp.loguniform(label='lr', low=np.log(5e-2), high=np.log(2e-1)),
              'lr_decay': hp.choice(label='lr_decay', options=[0.98,0.99,1]),
              'n_lr_decay_steps': hp.choice(label='n_lr_decay_steps', options=[1,2,4]),
@@ -79,6 +58,11 @@ def main(sample_size=None):
              'alpha_cl': hp.choice(label='alpha_cl', options=[0.01,0.1,1]),
              'n_epochs': hp.choice(label='n_epochs', options=[25]),
              'eval_steps': scope.int(hp.choice(label='eval_steps', options=[4])),}
+
+    if sample_size==None:
+        space['n_classes'] = hp.choice(label='n_classes', options=[4000])
+    else:
+        space['n_classes'] = hp.choice(label='n_classes', options=[sample_size])
 
     # Hyperparameters search
     trials = Trials()
@@ -89,4 +73,4 @@ def main(sample_size=None):
 # Cell
 if __name__ == "__main__":
     # os.chdir('nbs')
-    main(sample_size=100)
+    main(64, sample_size=100)
