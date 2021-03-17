ba12130c1f21b906b5a60a370958e91800cc31be
Ubuntu <ubuntu@ip-172-31-72-96.ec2.internal>
2021-03-17 19:09:55 +0000

    dataset class modified to normalize, flip and crop

--- hw2p2/datasets.py
+++ hw2p2/datasets.py
@@ -75,6 +75,7 @@ class FaceClassificationDataset(Dataset):
         if self.mode == 'train':
             trans_list = [transforms.Resize(80),
                           transforms.RandomCrop(64),
+                          transforms.RandomHorizontalFlip(),
                           transforms.ToTensor(),
                           transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                std=[0.229, 0.224, 0.225])]
